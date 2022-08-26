29b03cbc927de0ff795d9d8f86f54a15af3c3aad
dizzatron <murphybermingham@gmail.com>
2022-08-26 16:36:02 +0100

    Added SVG

--- DesktopOrg.py
+++ DesktopOrg.py
@@ -10,6 +10,7 @@ destinationPNG = "/Users/murph/Desktop/PNG"
 destinationWORD = "/Users/murph/Desktop/WORD"
 destinationJPG = "/Users/murph/Desktop/JPG"
 destinationURL = "/Users/murph/Desktop/Program Shortcuts"
+destinationSVG = "/Users/murph/Desktop/SVG"
 print(os.getcwd())
 for file in os.listdir():
 
@@ -31,4 +32,7 @@ for file in os.listdir():
 
     if ext == '.url':
         shutil.move(file, destinationURL)
+
+    if ext == '.svg':
+        shutil.move(file, destinationSVG)
 
